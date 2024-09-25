121668da9bae02dbe1964e4c4e82481f472d0b9e
Sheheryaar Ansar <sheryaarlong@gmail.com>
2024-09-26 03:28:39 +0500

    Fixed the bugs regarding the page `Shop`

--- src/app/components/shop/AddToCart.jsx
+++ src/app/components/shop/AddToCart.jsx
@@ -1,38 +1,36 @@
 import React from 'react'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 import { IoMdClose } from 'react-icons/io'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { cartDecrement, cartIncrement, removeCart } from '../../redux/features/cartSlices'
+import { MdDelete } from 'react-icons/md'
 
-const AddToCart = ({ id, img, name, price, qty, totalQty, totalItems }) => {
+const AddToCart = ({ id, img, name, price, qty }) => {
     const dispatch = useDispatch();
-    const total = totalQty.toLocaleString()
     return (
-        <div>
-            <div key={id} className='flex justify-center items-center mb-4 shadow-md relative'>
-                <div className='flex justify-center items-center'>
-                    <img src={img} alt={name} className='w-[60px] h-[60px] rounded-md' />
-                </div>
-                <div>
-                    <h1>{name}</h1>
-                    <div className='flex'>
-                        <p>{price.toLocaleString()}-/PKR</p>
-                        <div className='flex justify-center items-center gap-1 absolute right-7'>
-                            <AiOutlineMinus onClick={()=>dispatch(qty > 1 ? cartDecrement({id: id}) : dispatch(removeCart(id)))} className='border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all cursor-pointer' />
-                            <span>{qty}</span>
-                            <AiOutlinePlus onClick={()=>qty >= 1 ? dispatch(cartIncrement({id: id})) : qty = 0} className='border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all cursor-pointer' />
+        <div className='relative'>
+            <div className='z-50'>
+                <div key={id} className='flex mb-4 shadow-md'>
+                    <div>
+                        <img src={img} alt={name} className='w-[60px] h-[60px] rounded-md' />
+                    </div>
+                    <div>
+                        <div className='flex justify-between items-center w-full'>
+                            <h1>{name}</h1>
+                            <MdDelete  onClick={()=>dispatch(removeCart(id))} className='text-red-600 cursor-pointer absolute right-3 top-7 text-md' />
+                        </div>
+                        <div className='flex'>
+                            <p>{price.toLocaleString()} -/PKR</p>
+                            <div className='flex justify-center items-center gap-1 absolute right-7'>
+                                <AiOutlineMinus onClick={() => dispatch(qty > 1 ? cartDecrement({ id: id }) : dispatch(removeCart(id)))} className='border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all cursor-pointer' />
+                                <span>{qty}</span>
+                                <AiOutlinePlus onClick={() => qty >= 1 ? dispatch(cartIncrement({ id: id })) : qty = 0} className='border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-green-500 hover:border-none rounded-md p-1 text-xl transition-all cursor-pointer' />
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='absolute top-full bottom-0'>
-                    <div>
-                        <h1>Items: {totalItems}</h1>
-                        <h1>Total Amount: {total}</h1>
-                    </div>
-                    <hr />
-                    <button>Checkout</button>
-                </div>
             </div>
+
 
         </div>
     )
--- src/app/pages/Shop.jsx
+++ src/app/pages/Shop.jsx
@@ -5,17 +5,18 @@ import { data } from '../data'
 import AddToCart from '../components/shop/AddToCart';
 import { IoMdClose } from 'react-icons/io';
 import { cartSlider } from '../redux/features/cartsliderSlices';
+import { GiShoppingCart } from 'react-icons/gi';
 
 const Shop = () => {
     const cartItems = useSelector((state) => state.cart);
-    const active = useSelector((state)=>state.slider.slider)
-    const mode = useSelector((state)=>state.mode.mode)
+    const active = useSelector((state) => state.slider.slider)
+    const mode = useSelector((state) => state.mode.mode)
     const dispatch = useDispatch()
-    const totalItems = cartItems.reduce((totalQty, item)=>totalQty + item.qty, 0);
-    const totalQty = cartItems.reduce((total, item)=>total + item.qty * item.price, 0);
-    console.log(totalQty);
-    
-    
+    const totalItems = cartItems.reduce((totalQty, item) => totalQty + item.qty, 0);
+    const totalQty = cartItems.reduce((total, item) => total + item.qty * item.price, 0);
+    const total = totalQty.toLocaleString()
+
+
     return (
         <div>
             <div className='flex flex-wrap'>
@@ -32,12 +33,12 @@ const Shop = () => {
                     )
                 }) : <h1>No such Products</h1>}
             </div>
-            <div className={`fixed top-0 right-0 w-[400px] ${mode ? 'bg-black text-white' : 'bg-white text-black'} h-full transition-all duration-300 ${active  ?'translate-x-0' : 'translate-x-full'}`}>
+            <div className={`fixed top-0 right-0 w-[400px] ${mode ? 'bg-black text-white' : 'bg-white text-black'} h-full transition-all duration-300 ${active ? 'translate-x-0' : 'translate-x-full'} overflow-y-scroll`}>
                 <div className='flex justify-between items-center p-3'>
                     <span>Cart</span>
-                    <IoMdClose onClick={()=>dispatch(cartSlider())} className='border-2 border-gray-600 text-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-300 hover:border-red-300 cursor-pointer' />
+                    <IoMdClose onClick={() => dispatch(cartSlider())} className='border-2 border-gray-600 text-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-300 hover:border-red-300 cursor-pointer' />
                 </div>
-                {cartItems.length === 0 ? <h1>Empty Cart</h1> : cartItems.map((cart) => {
+                {cartItems.length === 0 ? <div className='flex justify-center items-center flex-col'><GiShoppingCart className='text-2xl'/> <h1>Your Cart Is Empty</h1> </div> : cartItems.map((cart) => {
                     return (
                         <AddToCart
                             id={cart.id}
@@ -45,11 +46,22 @@ const Shop = () => {
                             name={cart.name}
                             price={cart.price}
                             qty={cart.qty}
-                            totalItems={totalItems}
-                            totalQty={totalQty}
                         />
                     )
                 })}
+                {cartItems.length > 0 && (
+                    <div className={`pd-4 bg-black ${!mode ? 'bg-white text-black' : 'bg-black text-white'}`}>
+                        <div>
+                            <div>
+                                <h1>Items: {totalItems}</h1>
+                                <h1>Total Amount: {total} -/PKR</h1>
+                            </div>
+                            <hr />
+                            <button>Checkout</button>
+                        </div>
+                    </div>
+                )}
+
             </div>
         </div>
     )
