0bdef2b2094284efa678fe2f2e5b5d2df82520ea
Sheryaar <Sheryaarlong@gmail.com>
2024-10-07 13:14:19 +0500

    .

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import toast from 'react-hot-toast'
-import { clearCart } from '../redux/features/cartSlices'
 
 const Home = () => {
   
   return (
     <div className='pt-[100px] mt-[70px] min-h-screen'>
-      <button onClick={()=>dispatch(clearCart())}>Remove Cart Items</button>
     </div>
   )
 }
